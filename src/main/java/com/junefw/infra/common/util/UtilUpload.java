ac089c41a2d228df89faffd6ef05979bf7d072b3
de1bo <ogsal5765@naver.com>
2022-04-11 17:06:25 +0900

    year month day insert success

--- src/main/java/com/junefw/infra/common/util/UtilUpload.java
+++ src/main/java/com/junefw/infra/common/util/UtilUpload.java
@@ -18,18 +18,33 @@ public class UtilUpload {
 		String uuidFileName = uuid + "." + ext;
 		String pathModule = className;
 		String nowString = UtilDateTime.nowString();
-		String pathDate = nowString.substring(0,4) + "/" + nowString.substring(5,7) + "/" + nowString.substring(8,10);
+		String year = nowString.substring(0,4);
+		String month = nowString.substring(5,7);
+		String day = nowString.substring(8,10);
+		String pathDate = year + "/" + month + "/" + day;
 		String path = Constants.UPLOAD_PATH_PREFIX + "/" + pathModule + "/" + pathDate + "/";	
 		
 		createPath(path);
 		
 		multipartFile.transferTo(new File(path + uuidFileName));
 		
+		System.out.println("year::::::::::::::::" +year );
+		System.out.println("month::::::::::::::::" +month );
+		System.out.println("day::::::::::::::::" +day);
+		
+		dto.setYear(year);
+		dto.setMonth(month);
+		dto.setDay(day);
+		
 		
 		dto.setOriginalName(fileName);
 		dto.setUuidName(uuidFileName);
 		dto.setExt(ext);
 		dto.setSize(multipartFile.getSize());
+		
+		/*
+		 * dto.setYear(); dto.setMonth(); dto.setDay(pathDate);
+		 */
 	}
 	
 	// Createpath를 이용해서 날짜별로 폴더가 생성됨
--- src/main/java/com/junefw/infra/modules/store/Store.java
+++ src/main/java/com/junefw/infra/modules/store/Store.java
@@ -23,6 +23,10 @@ private String tableName;
 	private Integer delNy;
 	private String pseq;
 	
+	// image date 
+	private String year;
+	private String month;
+	private String day;
 	
 	// regmod
 	private String regIp;
@@ -398,6 +402,24 @@ private String tableName;
 	}
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
 	}
 	
 	
