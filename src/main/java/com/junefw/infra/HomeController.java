1e973b5685497c3ac3103e2fffd3a6d59188fd41
de1bo <ogsal5765@naver.com>
2022-05-17 16:21:35 +0900

    Mvc 패턴을 이용한 Rest api 공공데이터 활용

--- src/main/java/com/junefw/infra/HomeController.java
+++ src/main/java/com/junefw/infra/HomeController.java
@@ -1,8 +1,17 @@
 package com.junefw.infra;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +19,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Handles requests for the application home page.
@@ -35,5 +46,103 @@ public class HomeController {
 		
 		return "home";
 	}
+	
+	@RequestMapping(value = "/test/publicCorona1List")
+	public String publicCorona1List(Model model) throws Exception {
+		
+		String apiUrl = "http://apis.data.go.kr/1471000/CovidDagnsRgntProdExprtStusService/getCovidDagnsRgntProdExprtStusInq?serviceKey=OhsPQAXAkqO95cgktfOWeGTcYDs4JKiL08UQzHZO4cIptrbzPyOOTaQqyFuNS5WXzWfVOXEGZvYGYIlaV4aHhw%3D%3D&numOfRows=3&pageNo=1&type=json";
+		
+		URL url = new URL(apiUrl);
+		HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+		httpURLConnection.setRequestMethod("GET");
+		
+		BufferedReader bufferedReader;
+		if (httpURLConnection.getResponseCode() >=200 && httpURLConnection.getResponseCode() <= 300) {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+		} else {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getErrorStream()));
+		}
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		String line;
+		while ((line = bufferedReader.readLine()) != null) {
+			System.out.println("line: " + line);
+			stringBuilder.append(line);
+		}
+		
+		bufferedReader.close();
+		httpURLConnection.disconnect();
+		
+		System.out.println("stringBuilder.toString(): " +stringBuilder.toString());
+		
+//		json object + array string -> java map
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		Map<String, Object> map = objectMapper.readValue(stringBuilder.toString(), Map.class);
+		
+		for(String key :map.keySet()) {
+//			String value= map.get(key); // error casting
+//			String value = map.get(key).toString(); // error npe
+			String value= String.valueOf(map.get(key));	//ok
+			System.out.println("[key]: " + key + "[value]: " +value);
+		}
+		Map<String, Object> header = new HashMap<String, Object>();
+		header = (Map<String, Object>) map.get("header");
+		
+		System.out.println("############Header");
+		for( String key : header.keySet()) {
+			String value = String.valueOf(header.get(key));
+			System.out.println("[key]: " + key + "[value]: " +value);
+		}
+		
+		String aaa =(String) header.get("resultCode");
+		
+		System.out.println("header.get(\"resultCode\"): " + header.get("resultCode"));
+		System.out.println("header.get(\"resultMsg\"): " + header.get("resultMsg"));
+		
+		Map<String, Object> body = new HashMap<String, Object>();
+		body = (Map<String, Object>) map.get("body");
+		
+		List<Home> items = new ArrayList<Home>();
+		items = (List<Home>)body.get("items");
+		
+		for(int i = 0; i< items.size(); i++) {
+			
+		}
+		
+		return "test/publicCorona1List";
+	}
+	
+	@RequestMapping(value = "/test/publicCorona1JsonNodeList")
+	public String publicCorona1JsonNodeList(Model model) throws Exception{
+		
+		String apiUrl = "http://apis.data.go.kr/1471000/CovidDagnsRgntProdExprtStusService/getCovidDagnsRgntProdExprtStusInq?serviceKey=OhsPQAXAkqO95cgktfOWeGTcYDs4JKiL08UQzHZO4cIptrbzPyOOTaQqyFuNS5WXzWfVOXEGZvYGYIlaV4aHhw%3D%3D&numOfRows=3&pageNo=1&type=json";
+		
+		URL url = new URL(apiUrl);
+		HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+		httpURLConnection.setRequestMethod("GET");
+		
+		BufferedReader bufferedReader;
+		if (httpURLConnection.getResponseCode() >=200 && httpURLConnection.getResponseCode() <= 300) {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+		} else {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getErrorStream()));
+		}
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		String line;
+		while ((line = bufferedReader.readLine()) != null) {
+			System.out.println("line: " + line);
+			stringBuilder.append(line);
+		}
+		
+		bufferedReader.close();
+		httpURLConnection.disconnect();
+		
+		System.out.println("stringBuilder.toString(): " +stringBuilder.toString());
+		
+		return "test/publicCorona1List";
+	}
+	
 	
 }
